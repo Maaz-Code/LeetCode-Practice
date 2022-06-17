b695d2d265ac2c035971ab4537e74486a7005f99
Maaz Ahmed <ahmedmaaz73@gmail.com>
2022-06-17 13:24:19 +0530

    Time: 6 ms (60.05%), Space: 44.8 MB (39.99%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        
+        //Converting to character arrays and then sorting them;
+        char x[] = s.toCharArray();
+        char y[] = t.toCharArray();
+        Arrays.sort(x);
+        Arrays.sort(y);
+        
+        for(int i = 0; i < x.length; i++) {
+            if(x[i] != y[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
