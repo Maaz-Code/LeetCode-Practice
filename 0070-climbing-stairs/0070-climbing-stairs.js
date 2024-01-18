005130eb5f947577a95047fee8aaa6f5d6c45558
Maaz Ahmed <ahmedmaaz73@gmail.com>
2024-01-19 00:44:56 +0530

    Time: 60 ms (16.80%), Space: 41.8 MB (36.84%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let ans = [1,2];
+    for(let i = 2; i<n ; i++){
+        ans[i] = ans[i-1] + ans[i-2];
+    }
+    return ans[n-1];
+};
