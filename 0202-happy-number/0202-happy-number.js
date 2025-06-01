3667084523b331c8cc37cebe42dafb63277504b5
Maaz Ahmed <ahmedmaaz73@gmail.com>
2025-06-01 23:56:15 +0530

    Time: 1 ms (78.86%), Space: 53.8 MB (86.78%) - LeetHub

--- 0202-happy-number/0202-happy-number.js
+++ 0202-happy-number/0202-happy-number.js
@@ -2,21 +2,25 @@
  * @param {number} n
  * @return {boolean}
  */
-var isHappy = function(n) {
-    const set = new Set();
-    while(n !== 1){
-        if(set.has(n)){
-            return false;
-        }
-        set.add(n);
 
-        let sum = 0;
-        while(n > 0){
-            let i = n % 10;
-            sum = sum + i*i;
-            n = Math.floor(n/10);
-        }
-        n = sum;
+function getNext(n) {
+    let sum = 0;
+    while (n > 0) {
+        let digit = n % 10;
+        sum += digit * digit;
+        n = Math.floor(n / 10);
     }
-    return true;
+    return sum;
+}
+
+var isHappy = function(n) {
+    let slow = n;
+    let fast = getNext(n);
+
+    while(fast !== 1 && slow !== fast){
+        slow = getNext(slow);
+        fast = getNext(getNext(fast));
+    }
+
+    return fast === 1;
 };
