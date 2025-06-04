37bb8a1123df07ac3efede922baded411a7690e3
Maaz Ahmed <ahmedmaaz73@gmail.com>
2025-06-05 03:43:02 +0530

    Time: 0 ms (100%), Space: 63.2 MB (91.74%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -7,21 +7,6 @@ var rotate = function(nums, k) {
     let n = nums.length;
     k = k % n;
 
-    // Helper to reverse from start to end
-    function reverse(start, end) {
-        while (start < end) {
-            [nums[start], nums[end]] = [nums[end], nums[start]];
-            start++;
-            end--;
-        }
-    }
-
-    // Step 1: Reverse entire array
-    reverse(0, n - 1);
-
-    // Step 2: Reverse first k
-    reverse(0, k - 1);
-
-    // Step 3: Reverse rest
-    reverse(k, n - 1);   
+    let removed = nums.splice(n - k);  //last k elements
+    return nums.unshift(...removed);   //add them in front
 };
