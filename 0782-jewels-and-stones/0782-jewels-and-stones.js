d992bcb20b86e9f62ba7d7675d139a5026b54b94
Maaz Ahmed <ahmedmaaz73@gmail.com>
2025-08-02 23:57:09 +0530

    Time: 1 ms (49.02%), Space: 54 MB (50.42%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    const set = new Set(jewels);
+    let count = 0;
+    for(let i of stones){
+        if(set.has(i)){
+            count++;
+        }
+    }
+    return count;
+};
