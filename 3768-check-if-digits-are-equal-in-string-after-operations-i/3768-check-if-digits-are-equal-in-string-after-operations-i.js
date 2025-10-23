e3ba16e39c921b9378dd1cf1dc4d494a6f0b2c82
Maaz Ahmed <ahmedmaaz73@gmail.com>
2025-10-24 02:29:17 +0530

    Time: 5 ms (94.29%), Space: 58.9 MB (85.71%) - LeetHub

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const hasSameDigits = function (s) {
+    while (s.length > 2) {
+        let next = ""
+        for (let i = 0; i < s.length - 1; i++) {
+            let sum = (Number(s[i]) + Number(s[i + 1])) % 10;
+            next += sum
+        }
+        s = next;
+    }
+    return s[0] === s[1]
+};
