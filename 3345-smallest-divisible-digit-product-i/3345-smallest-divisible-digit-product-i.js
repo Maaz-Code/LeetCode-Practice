b9158f0383984f7783be3c76967843e534082d13
Maaz Ahmed <ahmedmaaz73@gmail.com>
2026-08-17 00:08:29 +0530

    Time: 1 ms (54.32%), Space: 56 MB (31.45%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @param {number} t
+ * @return {number}
+ */
+var smallestNumber = function(n, t) {
+    while(true){
+        let product = 1;
+        let temp = n;
+        while(temp > 0){
+            product = product * (temp%10);
+            temp = Math.floor(temp/10);
+        }
+        if(product % t === 0){
+            return n;
+        }
+        n = n + 1;
+    }
+};
